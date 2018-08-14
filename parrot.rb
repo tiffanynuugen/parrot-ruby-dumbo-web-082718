c953d83ac2d3ea3ac3c0e38f55e55821ddbff5e1
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 03:59:24 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 def parrot(phrase = "Squawk!")
-  puts "Squawk!"
-  "Pretty bird!"
+  puts "Pretty bird!"
+  "Squawk!"
 end
  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
