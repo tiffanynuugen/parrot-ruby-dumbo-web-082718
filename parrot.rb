99f75aa67e6546206ecbf5322cd1915c11d1d4b3
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 03:39:07 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(sound = "Squawk!")
+  puts "Squawk!"
+  return "Squawk!"
+end  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
